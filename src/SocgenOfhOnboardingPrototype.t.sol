6537adb6b9db3cddd55675a84517ab49a2cfd8ed
Henrique Barcelos <hbarcelos909@gmail.com>
2021-12-14 12:30:56 -0300

    dapp init SocgenOfhOnboardingPrototype

---
+++ src/SocgenOfhOnboardingPrototype.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.6;
+
+import "ds-test/test.sol";
+
+import "./SocgenOfhOnboardingPrototype.sol";
+
+contract SocgenOfhOnboardingPrototypeTest is DSTest {
+    SocgenOfhOnboardingPrototype prototype;
+
+    function setUp() public {
+        prototype = new SocgenOfhOnboardingPrototype();
+    }
+
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
+}
