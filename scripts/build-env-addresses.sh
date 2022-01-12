26cb68237eef158be864244424fa701422706698
canokaue <kauecano@gmail.com>
2022-01-12 11:05:38 -0300

    add ces-goerli support on build-env-addresses

--- scripts/build-env-addresses.sh
+++ scripts/build-env-addresses.sh
@@ -21,7 +21,7 @@ if [[ $_ != "${0}" ]]; then
 fi
 
 if [ -z "${1}" ]; then
-  echo "Please specify the network [ mainnet, goerli ] or a file path as an argument."
+  echo "Please specify the network [ mainnet, goerli, ces-goerli ] or a file path as an argument."
   [ -z "${PS1}" ] && exit || return
 fi
 
@@ -29,6 +29,8 @@ if [ "${1}" == "goerli" ]; then
   URL="https://changelog.makerdao.com/releases/goerli/active/contracts.json"
 elif [ "${1}" == "mainnet" ]; then
   URL="https://changelog.makerdao.com/releases/mainnet/active/contracts.json"
+elif [ "${1}" == "ces-goerli" ]; then
+  URL="https://raw.githubusercontent.com/ClioFinance/ces-goerli/master/contracts.json"
 else
   echo "# Invalid network ${1}"
   [ -z "${PS1}" ] && exit || return
