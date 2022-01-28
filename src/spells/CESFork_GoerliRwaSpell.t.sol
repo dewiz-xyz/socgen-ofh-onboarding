cc2c86f8b325884d541bb25611cdfece86998a5f
Nikolaj Lollike <nikolajzl@hotmail.com>
2022-01-28 13:13:04 +0100

    remove launch from test

--- src/spells/CESFork_GoerliRwaSpell.t.sol
+++ src/spells/CESFork_GoerliRwaSpell.t.sol
@@ -511,16 +511,6 @@ contract DssSpellTest is DSTest, DSMath {
         assertTrue(false, "TestError/GiveTokens-slot-not-found");
     }
     function vote(address spell_) internal {
-        
-        if (chief.live() == 0){
-            giveTokens(gov, 999999999999 ether);
-            gov.approve(address(chief), uint256(-1));
-            chief.lock(999999999999 ether);
-            address[] memory slate = new address[](1);
-            slate[0] = address(0);
-            chief.vote(slate);
-            chief.launch();
-        }
         if (chief.hat() != spell_) {
             giveTokens(gov, 999999999999 ether);
             gov.approve(address(chief), uint256(-1));
