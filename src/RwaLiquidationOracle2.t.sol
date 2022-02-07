e8c6823afe840329bb011bf8b8732f5713965151
Henrique Barcelos <hbarcelos909@gmail.com>
2022-02-07 14:13:21 -0300

    refactor: rename RWA files to match the main contract name

--- src/RwaLiquidationOracle2.t.sol
+++ src/RwaLiquidationOracle2.t.sol
@@ -32,9 +32,10 @@ import {AuthGemJoin} from "dss-gem-joins/join-auth.sol";
 import {MockOFH} from "./mock/MockOFH.sol";
 import {OFHTokenLike} from "./ITokenWrapper.sol";
 import {TokenWrapper} from "./TokenWrapper.sol";
-import {RwaInputConduit2, RwaOutputConduit2} from "./RwaConduits.sol";
-import {RwaUrn2} from "./RwaUrn.sol";
-import {RwaLiquidationOracle2} from "./RwaLiquidationOracle.sol";
+import {RwaInputConduit2} from "./RwaInputConduit2.sol";
+import {RwaOutputConduit2} from "./RwaOutputConduit2.sol";
+import {RwaUrn2} from "./RwaUrn2.sol";
+import {RwaLiquidationOracle2} from "./RwaLiquidationOracle2.sol";
 
 interface Hevm {
     function warp(uint256) external;
