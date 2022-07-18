0a3c0b5b7f2191da0bb968cafaf0d5cbb986ba5c
Henrique Barcelos <16565602+hbarcelos@users.noreply.github.com>
2022-07-18 19:24:36 -0300

    chore(deps): update mip21-toolkit

--- src/Import.sol
+++ src/Import.sol
@@ -6,7 +6,6 @@ import "mip21-toolkit/tokens/RwaToken.sol";
 import "mip21-toolkit/urns/RwaUrn2.sol";
 import "mip21-toolkit/conduits/RwaOutputConduit2.sol";
 import "mip21-toolkit/conduits/RwaInputConduit2.sol";
-import "mip21-toolkit/jars/RwaJar.sol";
 import "mip21-toolkit/oracles/RwaLiquidationOracle.sol";
 import "dss-gem-joins/join-auth.sol";
 import "forward-proxy/ForwardProxy.sol";
